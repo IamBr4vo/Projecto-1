e13c14ab3d4e663426a97df0cf88290de0adcd8e
2123i3fabri <reyesufabricio@gmail.com>
2023-07-06 20:44:55 -0600

    Actualizacion

--- Projecto1/src/main/java/Logica/Sedes.java
+++ Projecto1/src/main/java/Logica/Sedes.java
@@ -4,10 +4,38 @@
  */
 package Logica;
 
+import java.util.ArrayList;
 /**
  *
  * @author fabri
  */
 public class Sedes {
     
+ 
+    private ArrayList<String> ubicaciones;
+
+    public Sedes() {
+        ubicaciones = new ArrayList<>();
+        // Agregar ubicaciones de Estados Unidos
+        ubicaciones.add("Atlanta");
+        ubicaciones.add("Boston");
+        ubicaciones.add("Dallas");
+        ubicaciones.add("Houston");
+        ubicaciones.add("Kansas City");
+        ubicaciones.add("Los Angeles");
+        ubicaciones.add("Miami");
+        ubicaciones.add("Nueva York/Nueva Jersey");
+        ubicaciones.add("Filadelfia");
+        ubicaciones.add("San Francisco");
+        ubicaciones.add("Seattle");
+
+        // Agregar ubicaciones de México
+        ubicaciones.add("Guadalajara");
+        ubicaciones.add("Ciudad de México");
+        ubicaciones.add("Monterrey");
+
+        // Agregar ubicaciones de Canadá
+        ubicaciones.add("Vancouver");
+        ubicaciones.add("Toronto");
+    }   
 }
